17867305e65ecb4a2c5b270320e19b91a114bf29
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-29 10:59:11 +0100

    🚚 Comment router until have controller and repo

--- src/router/users.ts
+++ src/router/users.ts
@@ -2,10 +2,10 @@ import { Router } from 'express';
 
 export const usersRouter = Router();
 
-const controller = new UsersController(
-    UsersRepository.getInstance(),
-    PlacesRepository.getInstance()
-);
+// const controller = new UsersController(
+//     UsersRepository.getInstance(),
+//     PlacesRepository.getInstance()
+// );
 
-usersRouter.post('/register', controller.register.bind(controller));
-usersRouter.post('/login', controller.login.bind(controller));
+// usersRouter.post('/register', controller.register.bind(controller));
+// usersRouter.post('/login', controller.login.bind(controller));
