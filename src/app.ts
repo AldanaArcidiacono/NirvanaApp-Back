8afee6996006a218be942dd8ff64463260750d22
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-29 10:59:42 +0100

    ✨ Add app file

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { usersRouter } from './router/users.js';
+import { travelRouter } from './router/places.js';
+
+export const app = express();
+app.disable('x-powered-by');
+
+const corsOptions = {
+    origin: '*',
+};
+app.use(morgan('dev'));
+app.use(cors(corsOptions));
+app.use(express.json());
+
+//app.use(setCors);
+
+app.get('/', (_req, res) => {
+    res.send('API rest of travels. Write /travels to access').end();
+});
+
+app.use('/travels', travelRouter);
+app.use('/users', usersRouter);
+
+//app.use(errorManager);
