7afe5067c594b744d6aa1701259479f7095753e6
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-29 11:01:12 +0100

    ✨ Add db.connect

---
+++ src/db.connect.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import { USER, CLUSTER, PASSWORD } from './config.js';
+
+export function dbConnect() {
+    const DBName =
+        process.env.NODE_ENV !== 'test'
+            ? 'FinalProject'
+            : 'FinalProjectTesting';
+    let uri = `mongodb+srv://${USER}:${PASSWORD}`;
+    uri += `@${CLUSTER}/${DBName}?retryWrites=true&w=majority`;
+    return mongoose.connect(uri);
+}
