7c9b888cf36fed795796921c7e4cc54a1e41d21e
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-29 11:00:59 +0100

    ✨ Add config file

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const USER = process.env.USER;
+export const PASSWORD = process.env.PASSWORD;
+export const CLUSTER = process.env.CLUSTER;
+export const SECRET = process.env.SECRET;
